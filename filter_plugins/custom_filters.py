d404bf99ca5662b2aa0c960f23e0dcfdeeed8972
RyanOsborne <osborne@email.arizona.edu>
2023-10-27 06:00:30 -0700

    Edit

--- filter_plugins/custom_filters.py
+++ filter_plugins/custom_filters.py
@@ -1,4 +1,13 @@
 # custom_filters.py
+import netaddr
+
+def cidr_to_wildcard(cidr):
+    cidr_netmask = netaddr.IPNetwork(cidr).hostmask
+    # netmask = []
+    # for octet in cidr_netmask.split("."):
+    #     netmask.append(str(255 - int(octet)))
+    # return ".".join(netmask)
+    return cidr_netmask
 
 def netmask_subtract(cidr_netmask):
     netmask = []
